bd803bcd8e838f37b759daedc56515f615b111a6
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-02-20 18:20:15 -0800

    Update Program.cs

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Peliculas
 {
     class pelicula
     {
-        public string titulo;
-        public int año;
-        public string pais;
-        public string director;
-        public void imprime()
-        {
-            Console.WriteLine(titulo,año,pais,director);
-        }
+        private string titulo;
+        private int año;
+        private string pais;
+        private string director;
         public pelicula (string n,int a, string p,string d)
         {
             titulo=n;
@@ -20,17 +15,12 @@ namespace Peliculas
             pais=p;
             director=d;
         }
+        
     }
     class Program
     {
         static void Main(string[] args)
         {
-            List<pelicula> oscars = new List<pelicula>();
-            oscars.Add(new pelicula("Joker", 2019, "Estados Unidos", "Todd Phillips"));
-           
             
-            for(int i=0;i<oscars.Count;i++)
-            oscars[i].imprime();
-        }
     }
 }
