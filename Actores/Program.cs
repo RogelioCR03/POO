46d6dd61dae5ea32c293f12a1643d69f77377e44
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-03-06 12:54:50 -0800

    .

---
+++ Actores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actores
+{
+    class Peliculas
+    {
+       public string nombre;
+       public void imprime()
+       {
+           Console.WriteLine(nombre);
+       } 
+       public Peliculas(string p)
+       {
+           nombre = p;
+       }
+
+        public string actor;
+        public void imprimeActores()
+        {
+            Console.WriteLine(actor);
+        }
+        public AgregaActor(string a)
+        {
+            actor = a;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Peliculas> pelicula = new List<Peliculas>();
+            List<AgregaActor> actores = new List<AgregaActor>();
+            pelicula.Add(new Peliculas("Avengers"));
+            actores.add(new AgregaActor("Robert Dawney JR"));
+            actores [0].imprime();
+
+            foreach (Peliculas a in pelicula)
+            a.imprime();
+        }
+    }
+}
