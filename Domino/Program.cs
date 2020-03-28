d68626d8f698c15bc90a4612d724bb9ca765d312
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-03-28 15:40:44 -0700

    Update Program.cs

--- Domino/Program.cs
+++ Domino/Program.cs
@@ -2,11 +2,45 @@
 
 namespace Domino
 {
+    class Domino
+    {
+        public int arriba;
+        public int abajo;
+        public int total;
+
+        public Domino(int arr, int aba)
+        {
+            arriba = arr;
+            abajo = aba;
+            total = arriba + abajo;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}|{1}" + " La suma de esta ficha da " + "{2}", arriba, abajo, total);
+        }
+
+        public static Domino operator + (Domino x, Domino y)
+        {
+            int arriba = (x.arriba + y.arriba);
+            int abajo = (x.abajo + y.abajo);
+    
+            return new Domino(arriba,abajo);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Domino a = new Domino (5,6);
+            Domino b = new Domino (4,3);
+
+            Domino c = a+b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("El numero de puntitos resultantes es " + c);
+
         }
     }
 }
