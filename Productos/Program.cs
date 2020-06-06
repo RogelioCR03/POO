f4ca827b0521ce59d02cb754d50774ea5e608b7c
RogelioCR03 <60370979+RogelioCR03@users.noreply.github.com>
2020-06-05 19:23:16 -0700

    Edite proyecto

--- Productos/Program.cs
+++ Productos/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 
 namespace Productos
 {
-    class Producto
+    class Producto : IComparable
     {
         public Producto (string n, string d, int dep, string c, int l , double p)
         {
@@ -26,6 +26,10 @@ namespace Productos
         public int departamento {get; set;}
         public int likes {get; set;}
         public double precio;
+        public int CompareTo(object obj)
+        {
+            return likes.CompareTo(((Producto)obj).likes);
+        }
        
     }
     
@@ -70,12 +74,39 @@ namespace Productos
             return String.Format("{0}{1}{2}", DateTime.)
         }
     }*/
+
+    class ProductDB
+    {
+        public static void WriteToTXT(string path, List<Producto> Productos)
+        {
+            StreamWriter txtOut = new StreamWriter(new FileStream(path, FileMode.Create, FileAccess.Write));
+            foreach(Producto p in Productos)
+            {
+                txtOut.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}", p.nombre, p.descripcion, p.departamento, p.codigo, p.likes, p.precio);
+            }
+            txtOut.Close();
+        }
+        public static List<Producto> ReadFromTXT(string path)
+        {
+            List<Producto> Productos = new List<Producto>();
+            StreamReader txtIn = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+
+            while(txtIn.Peek()!= -1)
+            {
+                string line = txtIn.ReadLine();
+                string[] columns = line.Split('|');
+                Producto p = new Producto(columns[0],columns[1],Int32.Parse(columns[2]),columns[3],Int32.Parse(columns[4]),Double.Parse(columns[5]));
+                Productos.Add(p);
+            }
+            return Productos;
+        }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
-            List <Producto> Productos = new List<Producto>();
+            /*
             Productos.Add(new Producto("Balon", "Balon Nike del numero 5", 6, "SAHF5121", 89, 109.99));
             Productos.Add(new Producto("Agua natural", "Agua embotellada Bonafont", 9, "IENA7123", 14, 9.99));
             Productos.Add(new Producto("Cargador SAMSUNG", "Cargador carga rapida", 15, "GARQ1079", 57, 79.99));
@@ -86,11 +117,40 @@ namespace Productos
             Productos.Add(new Producto("Coca-Cola", "Refresco de cola", 9, "IENA1489", 38, 14.99));
             Productos.Add(new Producto("Crema Pond's", "Crema para la piel", 3, "GQEH3478", 28, 59.99));
             Productos.Add(new Producto("Cuchillos Cuisinart", "Juego de cuchillos para cocina", 2, "PQNF7813", 53, 349.99));
+            ProductDB.WriteToTXT("Listado.txt", Productos);
+            */
+            
+            int instruccion;
 
+            Console.WriteLine("¿Que desea saber?");
+            Console.WriteLine("1.- Lista de precios completa");
+            Console.WriteLine("2.- Productos por departamento");
+            Console.WriteLine("3.- Orden de likes");
+            instruccion = Int32.Parse(Console.ReadLine());
 
-            foreach (Producto p in Productos)
-            Console.WriteLine("{0} | {1} | Departamento: {2} | Codigo: {3} | Likes: {4} | Precio: {5}", p.nombre, p.descripcion, p.departamento, p.codigo, p.likes, p.precio);
-        
+            if (instruccion == 1)
+            {
+                List<Producto> Productos = new List<Producto>();
+                Productos = ProductDB.ReadFromTXT(@".\Listado.txt");
+                foreach(Producto p in Productos)
+                Console.WriteLine("Producto: {0}|{1}|Dep: {2}|Codigo: {3}|Likes: {4}|Precio: {5}$", p.nombre, p.descripcion, p.departamento, p.codigo, p.likes, p.precio);
+            }
+            else if (instruccion == 2)
+            {
+                List<Producto> Productos = new List<Producto>();
+                Productos = ProductDB.ReadFromTXT(@".\Listado.txt");
+                foreach(Producto p in Productos)
+                Console.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}", p.nombre, p.descripcion, p.departamento, p.codigo, p.likes, p.precio);
+            }
+            else if (instruccion == 3)
+            {
+                List<Producto> Productos = new List<Producto>();
+                Productos = ProductDB.ReadFromTXT(@".\Listado.txt");
+                Productos.Sort();
+                Productos.Reverse();
+                foreach(Producto p in Productos)
+                Console.WriteLine("Producto: {1}|{2}|Dep: {3}|Codigo: {4}|Likes: {0}|Precio: {5}$", p.likes, p.nombre, p.descripcion, p.departamento, p.codigo, p.precio);
+            }
         }
     }
 }
